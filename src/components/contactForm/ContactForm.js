523b6e409bf7aef506267121e7b3dae3ab06d824
EndrXOriginial <chris.pin@hotmail.ca>
2023-05-02 15:56:48 -0400

    completion of the form component

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -12,11 +12,11 @@ export const ContactForm = ({
   return (
     <form onSubmit={handleSubmit}>
       <label for="name">Name</label>
-      <input id="name" name="name" type="text" value={name} onChange={setName}/>
+      <input id="name" name="name" type="text" value={name} onChange={setName} required/>
       <label for="phone">Phone</label>
-      <input id="phone" name="phone" type="text" value={phone} onChange={setPhone} />
+      <input id="phone" name="phone" type="text" value={phone} onChange={setPhone} pattern="\(\d{3}\) \d{3}-\d{4}" required/>
       <label for="email">Email</label>
-      <input id="email" name="email" type="text" value={email} onChange={setEmail} />
+      <input id="email" name="email" type="text" value={email} onChange={setEmail} required/>
       <button>Submit</button>
     </form>
   );
