11943362f889e8092d9947cdc0300790ef8e693d
EndrXOriginial <chris.pin@hotmail.ca>
2023-05-02 17:36:47 -0400

    few little updates

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {Tile} from '../tile/Tile';
 
 export const TileList = (props) => {
   return (
     <div>
-      
+      {props.objectList.map(obj => {
+        <Tile name={obj.name} description={obj.description} />
+      })}
     </div>
   );
 };
